694d0bd04eaa763d1c259c6bdeea8d6b18c6020a
averyhs <avery2718.jpi@gmail.com>
2022-10-20 14:16:36 +0200

    Add UNet class defining architecture

--- segmentation/UNet.py
+++ segmentation/UNet.py
@@ -147,3 +147,50 @@ class Bottleneck(nn.Module):
         x = self.conv(inputs)
         return x
 
+
+###########
+## U-NET ##
+###########
+
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+        
+        # Encoder
+        self.e1 = EncoderBlock(1, 64)
+        self.e2 = EncoderBlock(64, 128)
+        self.e3 = EncoderBlock(128, 256)
+        self.e4 = EncoderBlock(256, 512)
+        
+        # Bottleneck
+        self.b = Bottleneck(512, 1024)
+        
+        # Decoder
+        self.d1 = DecoderBlock(1024, 512)
+        self.d2 = DecoderBlock(512, 256)
+        self.d3 = DecoderBlock(256, 128)
+        self.d4 = DecoderBlock(128, 64)
+        
+        # Classifier
+        self.outputs = nn.Conv2d(64, 1, kernel_size=1, padding=0)
+    
+    def forward(self, inputs):
+        
+        # Encoder
+        s1, p1 = self.e1(inputs)
+        s2, p2 = self.e2(p1)
+        s3, p3 = self.e3(p2)
+        s4, p4 = self.e4(p3)
+        
+        # Bottleneck
+        b = self.b(p4)
+        
+        # Decoder
+        d1 = self.d1(b, s4)
+        d2 = self.d2(d1, s3)
+        d3 = self.d3(d2, s2)
+        d4 = self.d4(d3, s1)
+        
+        # Classifier
+        outputs = self.outputs(d4)
+        return outputs
