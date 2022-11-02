8cfffa5917ff3e71b9cca512eda91f03e8ef8c09
averyhs <avery2718.jpi@gmail.com>
2022-11-02 09:40:11 +0200

    Update tests

--- segmentation/segscript.py
+++ segmentation/segscript.py
@@ -13,10 +13,25 @@ from time import process_time
 import numpy as np
 import datetime
 
-from params import  ROOT_DIR
+datadir = '../../SampleData/VerySmallExample'
 
-datadir = 'Code/SampleData/VerySmallExample/COCOOutput-simulate'
-# Structure or CellSium COCOOutput dir:
+# Structure of data directory:
+#     SampleData <all_data>
+#     ├─── VerySmallExample <datadir>
+#     |    ├─── images
+#     |    |   ├─── 000000000000.png
+#     |    |   ├─── 000000000001.png
+#     |    |   ├─── 000000000002.png
+#     |    |   ├─── ...
+#     |    |   └─── xxxxxxxxxxxx.png
+#     |    ├─── annotations.json
+#     |    ├─── masks.npz             # may not be here if it has not yet been generated
+#     |    └─── VerySmallExample.zip
+#     ├─── <other_datadir>
+#     └─── ...
+
+
+# Structure of CellSium COCOOutput dir:
 #     COCOOutput-simulate
 #     ├─── annotations.json        # COCO formatted JSON file
 #     └─── train                   # simulated microscope images
@@ -26,35 +41,53 @@ datadir = 'Code/SampleData/VerySmallExample/COCOOutput-simulate'
 #          ├─── ...
 #          └─── xxxxxxxxxxxx.png
 
-# Read the images <- X
-# --------------------
+# Read the images (X)
+# -------------------
 print('Reading images')
-images = list(io.ImageCollection(os.path.join(ROOT_DIR, datadir, 'train/*'), conserve_memory=True))
+images = list(io.ImageCollection(os.path.join(datadir, 'images/*'), conserve_memory=True))
 
-# Read the ground truth and Make the masks <- Y
-# ---------------------------------------------
-print('Creating masks')
-mask_filepath = os.path.join(ROOT_DIR, datadir, 'masks.npz')
+# Read the ground truth and 
+# Read or create masks (Y)
+# -------------------------
+masks_filepath = os.path.join(datadir, 'masks.npz')
+if os.path.exists(masks_filepath):
+    print('Reading masks')
+    masks = np.load(masks_filepath)
+    masks = [masks[x] for x in masks.files]
+else:
+    print('Creating masks')
+    coco = Data.read_coco_file(os.path.join(datadir, 'annotations.json'))
+    masks = Data.coco_to_masks(coco, masks_filepath)
 
-# # No masks exist yet:
-# coco = dataproc.read_coco_file(os.path.join(ROOT_DIR, datadir, 'annotations.json'))
-# masks = dataproc.coco_to_masks(coco, mask_filepath)
-
-# Masks exist:
-masks = np.load(mask_filepath)
-masks = [masks[x] for x in masks.files]
-
-# masks = list(x[:,:,0] for x in masks) # For the basic original U-Net we only want the basic cell masks actually
+masks = [masks[x][:,:,1] for x in range(len(masks))] # single channel for now
 
 # Data
 # ----
 print('Processing and loading data')
 data = Data(images, masks)
 
-data.unpatch()
+# # see an example
+# io.imsave('test-1_X.png', data.X[5])
+# io.imsave('test-1_Y.png', data.Y[5][:,:,1])
 
-plt.imshow(data.X[0])
-plt.show()
+# # Test that unpatch runs without errors
+# data.unpatch()
 
-plt.imshow(data.Y[0])
-plt.show()
+# # Test patch and unpatch 
+# # (not thorough, just quick check that images seem to be getting split and put back correctly)
+# io.imsave('test-2_X.png', data.X[-1])
+# io.imsave('test-2_Y.png', data.Y[-1][:,:,2])
+
+# Model
+# -----
+print('Initializing a model')
+model = Model() # create a Model instance
+
+# Train
+# ------
+print('Commence Training!')
+t0 = process_time()
+model_out = model.train(data.dataloader)#, savefile='model.pt', recordfile='records.npz')
+t1 = process_time()
+
+print('Training time: {time}'.format(time=datetime.timedelta(seconds=t1-t0)))
